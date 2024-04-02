d2c452157fe0c5e65ced0b1029605abeaa44f560
e1q23079 <156163994+e1q23079@users.noreply.github.com>
2024-04-03 02:30:12 +0900

    投稿機能の追加

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -5,4 +5,6 @@ app_name = 'app'
 
 urlpatterns= [
     path('',views.IndexView.as_view(),name='index'),
+    path('create/', views.articleCreateView.as_view(), name='article_create'),
+    path('create/complete/', views.articleCreateCompleteView.as_view(), name='article_create_complete'),
 ]
